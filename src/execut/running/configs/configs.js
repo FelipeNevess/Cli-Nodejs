3e4a5b2d3ebe1820a3a6cd35228e29af11a7dd25
felipenevess <felipe012neves@gmail.com>
2022-05-27 20:40:03 -0300

    feat: checking if a git object exists

--- src/execut/running/configs/configs.js
+++ src/execut/running/configs/configs.js
@@ -1,7 +1,8 @@
 const exec = require('../../exec/exec');
 
-module.exports = (configs, bash) => {
-  const arrayConfig = Object.values(configs);
+module.exports = (configs, bash, git) => {
+  const arrayConfig = Object
+    .values(git ? {...git, ...configs } : configs);
 
   arrayConfig.forEach((script) => {
     exec(`cd ${bash} && ${script}`);
--- src/execut/running/running.js
+++ src/execut/running/running.js
@@ -1,9 +1,9 @@
 const runConfig = require('./configs/configs');
 const runLibs = require('./libs/libs');
 
-function runningScript({ configs, libs }, bash) {
+function runningScript({ configs, libs, git }, bash) {
   console.log('### Configurando ambiente ###');
-  runConfig(configs, bash);
+  runConfig(configs, bash, git);
 
   console.log('### Instalando dependências ###');
   runLibs(libs, bash);
