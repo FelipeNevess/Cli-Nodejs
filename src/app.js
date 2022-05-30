37fcf71199c0e6eea5792a86d35698da45f932fd
felipenevess <felipe012neves@gmail.com>
2022-05-29 21:15:35 -0300

    feat: implementing to the function runningAdditionalCommands

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const { runningScript, exec } = require('./execut');
 const { start, get } = require('prompt');
 
 const processing = require('./processing');
-const writeScript = require('./writingScript');
+const { writeScript, runningAdditionalCommands } = require('./writingScript');
 const schema = require('./validations');
 
 start();
@@ -21,8 +21,10 @@ function getStated() {
       await writeScript(data, name);
 
       console.log('-- Instalando dependências...');
-      
       exec(`cd ${name} && npm i`);
+
+      console.log('-- Running additional commands...');
+      runningAdditionalCommands(data, name);
     } catch(err) {
       console.error(err);
     }
