1f962d42ec840996f18163af4429b921d2ee8d2a
felipenevess <felipe012neves@gmail.com>
2022-05-29 21:14:56 -0300

    feat: adding run command to typescript object

--- src/processing/useTypescript/index.js
+++ src/processing/useTypescript/index.js
@@ -1,10 +1,13 @@
 const { javascript, typescript } = require('../../packages');
+const { commandsExecutables } = require('../../commands');
 const { testRegex } = require('../../utils');
 
 module.exports = (useType) => {
   const useTypescript = testRegex(useType);
 
-  const response = useTypescript ? javascript : typescript;
+  const response = useTypescript ? javascript : {
+    ...typescript, ...commandsExecutables
+  };
 
   return response;
 }
